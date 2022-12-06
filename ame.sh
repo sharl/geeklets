2db32e449fac7fd3bb87b7ee6b8749c1376509bf
Sharl Morlaroll <sharl@hauN.org>
2022-12-07 05:16:58 +0900

    support rainsnow, lightning mode

--- ame.sh
+++ ame.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
+while getopts :st option; do
+    case $option in
+	s)
+	    mode=rainsnow
+	    shift
+	    ;;
+	t)
+	    mode=lightning
+	    shift
+	    ;;
+    esac
+done
+
 if [ -z "$1" ]; then
     loc=日本
     zom=4
@@ -18,7 +31,7 @@ lat=$(cat $tmpfile | sed -e 's,<lat>\(.*\)</lat>,\1,p;d')
 lng=$(cat $tmpfile | sed -e 's,<lng>\(.*\)</lng>,\1,p;d')
 
 if [ ! -z "${lat}" -a ! -z "${lng}" ]; then
-    url="https://weather.yahoo.co.jp/weather/zoomradar/?lat=${lat}&lon=${lng}&z=${zom}"
+    url="https://weather.yahoo.co.jp/weather/zoomradar/${mode}?lat=${lat}&lon=${lng}&z=${zom}"
     ogi=$(curl -s ${url} | sed -e 's,^.*<meta content="\(.*\)" property="og:image"/>.*$,\1,p;d' | sed -e 's/\&amp;/\&/g')
     curl -s "${ogi}" | img2sixel
 else
